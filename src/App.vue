ad3db1d03e8d4c6abe9fc964758eed8878dc00fc
Rehan Mulakhel <rehan.mulakhel@unifr.ch>
2024-05-22 18:01:21 +0200

    Add debounce and fix issue with vuejs reactivity.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref, watch } from 'vue'
+import { watchDebounced } from '@vueuse/core'
 import LanguageList from './components/LanguageList.vue'
 import PageFooter from './components/PageFooter.vue'
 import PageHeader from './components/PageHeader.vue'
@@ -9,42 +10,67 @@ import TablePagination from './components/TablePagination.vue'
 import { useFetch } from './fetch.js'
 
 const BASE_URL = 'https://eddb.unifr.ch/slzcaa-admin/items/materials'
+const DEBOUNCE_TIME = 512
+const DEBOUNCE_MAX_WAITE = 1024
 const NB_ELEMENT_PER_PAGE = 50
 
+const filterParam = ref('')
 const page = ref(1)
 const nbRowsFound = ref(0)
 const nbRowsInDatabase = ref(0)
-const rows = ref([])
-const urlDataTable = ref(generateUrlTable(page.value))
+const urlDataTable = ref(`${BASE_URL}?limit=${NB_ELEMENT_PER_PAGE}&page=${page.value}`)
+const urlNbFound = ref(`${BASE_URL}?aggregate[count]=*`)
+const urlNbTotal = ref(`${BASE_URL}?aggregate[count]=*`)
 
-const { data: dataCountTotal, error: errorCountTotal } = useFetch(generateUrlCountTotal())
-const { data: dataTable, error: tableError } = useFetch(urlDataTable)
+const { data: dataCountFound, error: errorCountFound } = useFetch(urlNbFound)
+const { data: dataCountTotal, error: errorCountTotal } = useFetch(urlNbTotal)
+const { data: dataTable, error: errorTable } = useFetch(urlDataTable)
 
-watch(page, () => {
-  urlDataTable.value = generateUrlTable(page.value)
+watch(dataCountFound, () => {
+  if (dataCountFound.value?.data[0]) {
+    nbRowsFound.value = dataCountFound.value.data[0].count
+  }
 })
 
 watch(dataCountTotal, () => {
-  nbRowsInDatabase.value = dataCountTotal._rawValue.data[0].count
+  if (dataCountTotal.value?.data[0]) {
+    nbRowsInDatabase.value = dataCountTotal.value.data[0].count
+  }
 })
 
-watch(dataTable, () => {
-  rows.value = dataTable._rawValue?.data
-})
+watchDebounced(
+  [filterParam, page],
+  () => {
+    let url = BASE_URL
+    url += `?limit=${NB_ELEMENT_PER_PAGE}`
+    url += `&page=${page.value}`
+    if (filterParam.value) {
+      url += `&filter=${filterParam.value}`
+    }
+    urlDataTable.value = url
+  },
+  { debounce: DEBOUNCE_TIME, maxWait: DEBOUNCE_MAX_WAITE }
+)
+
+watchDebounced(
+  filterParam,
+  () => {
+    if (filterParam.value) {
+      urlNbFound.value = `${BASE_URL}?filter=${filterParam.value}&aggregate[count]=*`
+    } else {
+      nbRowsFound.value = nbRowsInDatabase.value
+    }
+  },
+  { debounce: DEBOUNCE_TIME, maxWait: DEBOUNCE_MAX_WAITE }
+)
+
+function changeFilter(param) {
+  filterParam.value = param
+}
 
 function changePage(newPage) {
   page.value = newPage
-}
-
-function generateUrlCountTotal() {
-  return `${BASE_URL}?aggregate[count]=*`
-}
-
-function generateUrlTable(page) {
-  let base = BASE_URL
-  base += `?limit=${NB_ELEMENT_PER_PAGE}`
-  base += `&page=${page}`
-  return base
+  window.scrollTo({ top: 0 })
 }
 </script>
 
@@ -54,27 +80,29 @@ function generateUrlTable(page) {
     <LanguageList />
     <div class="grid grid-cols-1 gap-8 md:grid-cols-[1fr_2fr]">
       <div>
-        <FiltersNav />
+        <FiltersNav @filter="changeFilter" />
       </div>
       <div>
         <div role="alert" class="alert">
           <p>
             {{ 1 + (page - 1) * NB_ELEMENT_PER_PAGE }}
             to
-            {{ Math.min(page * NB_ELEMENT_PER_PAGE, nbRowsInDatabase) }}
-            of XXXX entries (filtered from {{ nbRowsInDatabase }} total entries)
+            {{ Math.min(page * NB_ELEMENT_PER_PAGE, nbRowsFound) }}
+            of {{ nbRowsFound }} entries (filtered from {{ nbRowsInDatabase }} total entries)
           </p>
         </div>
-        <div v-if="tableError">Oops! Error encountered: {{ tableError.message }}</div>
-        <div v-else-if="rows">
-          <SimpleTable :rows="rows" />
+        <div v-if="errorTable">Oops! Error encountered: {{ errorTable.message }}</div>
+        <div v-else-if="dataTable">
+          <SimpleTable :rows="dataTable.data" />
           <TablePagination
             :page="page"
-            :nbPage="Math.ceil(nbRowsInDatabase / NB_ELEMENT_PER_PAGE)"
+            :nbPage="Math.ceil(nbRowsFound / NB_ELEMENT_PER_PAGE)"
             @change-page="changePage"
           />
         </div>
-        <div v-else>Loading...</div>
+        <div v-else>
+          <div class="skeleton h-128 w-full"></div>
+        </div>
       </div>
     </div>
   </main>
